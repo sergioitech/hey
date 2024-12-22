4dd5080b0b72734ac824f4c5d78feabb3cb6e69b
sergioitech <sergenkina1@gmail.com>
2024-12-22 01:26:57 +0100

    Update hey.c

--- hey.c
+++ hey.c
@@ -1,40 +1,24 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="exme" />
-		<Option execution_dir="C:/Users/serge/OneDrive/TELECOM/PROGRAMMATION/PROJECT C TEL/exme/" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/exme" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/exme" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-		</Compiler>
-		<Unit filename="../OneDrive/TELECOM/PROGRAMMATION/PROJECT C TEL/exme/main.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Extensions />
-	</Project>
-</CodeBlocks_project_file>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+
+{
+    int nb1, nb2;
+
+
+    printf("\n   +++                 +++           +++++++++++++       ++++++               ++++++              \n");
+    printf("     +++                 +++           ++++++++++++++       ++++               ++++            |     \n");
+    printf("     +++                 +++           +++                   ++++              ++++            |     \n");
+    printf("     +++                 +++           +++                    ++++           ++++              |      \n");
+    printf("     +++                 +++           +++                     ++++         ++++               |       \n");
+    printf("     +++                 +++           ++++++++++++++            ++++     ++++                 |        \n");
+    printf("     +++++++++++++++++++++++           ++++++++++++++             ++++++++++                   |         \n");
+    printf("     +++++++++++++++++++++++           +++                          ++++++                     |          \n");
+    printf("     +++                 +++           +++                            +++                      |           \n");
+    printf("     +++                 +++           +++                            +++                      |            \n");
+    printf("     +++                 +++           +++++++++++++                  +++                      |            \n");
+     printf("    +++                 +++           +++++++++++++                 +++++                      .              \n");
+
+    return 0;
